9b205d7f70d214b0d52dd5c15ce79569aaa0ee88
Joaquin Avila <joaquinavila@alu.frt.utn.edu.ar>
2026-05-20 19:29:37 -0300

    Resolucion Caso List

--- Dsw2026Ej11/Tests/Ejemplos.cs
+++ Dsw2026Ej11/Tests/Ejemplos.cs
@@ -1,4 +1,6 @@
 ﻿namespace Dsw2026Ej11.Tests;
+using Dsw2026Ej11.Collections;
+using Dsw2026Ej11.Domain;
 
 
 internal class Ejemplos
@@ -11,7 +13,35 @@ internal class Ejemplos
     //Eliminar el primer elemento de la lista y listar por consola los alumnos
     public static void EjemploList()
     {
+        var a1 = new Alumno(1, "Joaquin", 8.0);
+        var a2 = new Alumno(2, "Lautaro", 7.0);
+        var a3 = new Alumno(3, "Tomas", 8.3);
 
+        var alumnos = new CasoList();
+
+        alumnos.Agregar(a1);
+        alumnos.Agregar(a2);
+        alumnos.Agregar(a3);
+
+        foreach (var a in alumnos.Listar())
+            Console.WriteLine($"{a}");
+
+        Console.WriteLine($"\n{alumnos.BuscarAlumno("Lautaro")}");
+
+        if (alumnos.BuscarAlumno("Pedro") == null)
+            Console.WriteLine($"\nNo existe");
+        else
+            Console.WriteLine($"{alumnos.BuscarAlumno("Pedro")}");
+
+        Console.WriteLine("\nEliminando el alumno a2.");
+        alumnos.Eliminar(a2);
+        foreach (var a in alumnos.Listar())
+            Console.WriteLine($"{a}");
+
+        Console.WriteLine("\nEliminando el primer elemento.");
+        alumnos.EliminarEn(0);
+        foreach (var a in alumnos.Listar())
+            Console.WriteLine($"{a}");
     }
 
     //Agregar 3 alumnos al diccionario
